f6216d37fadb45ba9725cf191be3aa7b8313ac32
kwonyong-hwi <130128640+kwonyong-hwi@users.noreply.github.com>
2023-06-18 12:12:20 +0900

    .env에서 값 받아오도록 수정

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -2,13 +2,13 @@ import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, browserSessionPersistence } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDVheOkbRXZU-eXaJ8OXRWfZtBelLsjFnQ",//process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: "capstone-dab03.firebaseapp.com",//process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    projectId: "capstone-dab03",//process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: "capstone-dab03.appspot.com",//process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: "292620228796",//process.env.REACT_APP_FIREBASE_SENDER_ID,
-    appId: "1:292620228796:web:8c33719b97efd619c158b9",//process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: "G-9PK1LK8PZQ"//process.env.REACT_APP_FIREBASE_MEASURMENT_ID
+    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID
 };
 
 // Firebase 앱 초기화
