038a76f87995a85e515a7b12f1d20b5556042d52
kyh <990225yh@gmail.com>
2023-07-07 18:21:32 +0900

    규칙 db화에 따른 firebase api 수정

--- front/src/firebase.jsx
+++ front/src/firebase.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth, signInWithEmailAndPassword, browserSessionPersistence } from 'firebase/auth';
+import { getDatabase, ref, set, push, remove } from 'firebase/database';
 
 const firebaseConfig = {
     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
@@ -12,13 +13,37 @@ const firebaseConfig = {
     measurementId: process.env.REACT_APP_FIREBASE_MEASURMENT_ID
 };
 
-// Firebase 앱 초기화
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig); // Firebase 앱 초기화
 
-// Auth 인스턴스 생성
 const auth = getAuth(app);
 
-// 세션 지속성을 '세션'으로 설정
-auth.setPersistence(browserSessionPersistence);
+auth.setPersistence(browserSessionPersistence);// 세션 지속성을 '세션'으로 설정
 
-export { auth, signInWithEmailAndPassword };
+const database = getDatabase();// 데이터베이스 인스턴스 생성
+
+// 데이터 파이어베이스에 추가
+const addItemToFirebase = async (item) => {
+  const rulesRef = ref(database, 'rule');
+  const newRuleRef = push(rulesRef);
+  const ruleId = newRuleRef.key;
+  
+  const newItem = {
+    ...item,
+    id: ruleId, // 새로운 룰의 ID 자동 할당
+  };
+  
+  await set(newRuleRef, newItem);
+  console.log('데이터를 Firebase Realtime Database에 추가했습니다.');
+};
+
+const deleteItemFromFirebase = async (ruleId) => {
+  try {
+    const ruleRef = ref(database, `rule/${ruleId}`);
+    await remove(ruleRef);
+    console.log('데이터를 Firebase Realtime Database에서 삭제했습니다.');
+  } catch (error) {
+    console.error('Error deleting data from Firebase:', error);
+  }
+};
+ 
+export { auth, signInWithEmailAndPassword, addItemToFirebase, deleteItemFromFirebase };
